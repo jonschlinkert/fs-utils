185e9e49e0ef9ec88e369d6ea95b56e8d7804572
Jon Schlinkert <dev@sellside.com>
2014-02-04 17:33:25 -0500

    v0.3.3
    adds "expand" methods for globbing. minor reorganization, improved code comments.

--- index.js
+++ index.js
@@ -8,15 +8,15 @@
 
 // Node.js
 var fs     = require('graceful-fs');
-var path   = require('path');
 var os     = require('os');
+var path   = require('path');
 
 // node_modules
-var paths  = require('path-utils');
 var async  = require('async');
-var rimraf = require('rimraf');
 var glob   = require('globule');
+var rimraf = require('rimraf');
 var YAML   = require('js-yaml');
+var _path  = require('path-utils');
 var _      = require('lodash');
 
 
@@ -61,6 +61,31 @@ _fs.stripBOM = function(str) {
   }
   return contents;
 };
+
+
+
+/**
+ * Stats
+ */
+
+_fs.getStatsSync = function (filepath) {
+  try {
+    return fs.statSync(filepath);
+  } catch (err) {
+    err.message = 'Failed to retrieve "' + filepath + '" stats: ' + err.message;
+    throw err;
+  }
+};
+
+_fs.getStats = function (filepath, callback) {
+  try {
+    return fs.stat(filepath, callback);
+  } catch (err) {
+    err.message = 'Failed to retrieve "' + filepath + '" stats: ' + err.message;
+    return callback(err, null);
+  }
+};
+
 
 
 /**
@@ -139,34 +164,93 @@ _fs.readDataSync = function(filepath, options) {
 
 
 /**
- * Expand (glob / minimatch)
+ * Expand (globbing / minimatch)
+ * Most of these methods are a thin wrapper around globule
  */
 
+// Returns the resolved filepath for a specific file using
+// globbing patterns. If multiple matches are found, only
+// the first is returned
+_fs.findFile = function(filepath, options) {
+  var opts = _.extend({filter: 'isFile'}, options);
+  var str = glob.find(filepath, opts)[0];
+  return str ? String(path.resolve(str)) : null;
+};
+
+// Returns resolved dirpath for a specific directory using
+// globbing patterns. If multiple matches are found, only
+// the first direc returned
+_fs.findDir = function(dir, options) {
+  var opts = _.extend({filter: 'isDirectory'}, options);
+  var str = glob.find(dir, opts)[0];
+  return str ? String(path.resolve(str)) : null;
+};
+
+// Returns a unique array of all file or directory paths
+// that match the given globbing pattern(s).
+_fs.expand = function(patterns, options) {
+  var opts = _.extend({}, options);
+  return glob.find(patterns, opts)
+};
+
+// Given a set of source file paths, returns an array
+// of src-dest file mapping objects. Example:
+//   _fs.mapping(['a.js', 'b.js', 'c.js'])
+_fs.mapping = function(filepaths, options) {
+  var opts = _.extend({}, options);
+  opts.srcBase = opts.cwd;
+  return glob.findMapping(filepaths, opts);
+};
+
+// Returns a unique array of all directories that match
+// the given globbing patterns.
+_fs.expandMapping = function(patterns, options) {
+  var opts = _.extend({}, options);
+  opts.srcBase = opts.cwd;
+  return glob.findMapping(patterns, opts);
+};
+
+// Match one or more globbing patterns against one
+// or more file paths.
+_fs.match = function(patterns, filepaths, options) {
+  var opts = _.extend({}, options);
+  return glob.match(patterns, filepaths, opts)
+};
+
+// Returns `true` if any files were matched
+_fs.isMatch = function(patterns, filepaths, options) {
+  var opts = _.extend({}, options);
+  return glob.isMatch(patterns, filepaths, opts)
+};
+
 /**
+ * Expand AND read JSON and YAML files.
  * @param  {String} filepath The filepath to read or string pattern to expand then read
  * @param  {Object} options  Object of options. 'namespace' will use the basename of
  *                           the source file as the name of the returned object
  * @return {Object}          Object of metadata
  */
-_fs.expandDataFiles = function (filepath, options) {
+_fs.expandData = function (filepath, options) {
   var opts = _.extend({}, options);
   opts.data = opts.data || {};
   var contents;
 
-  glob.find(filepath, opts).map(function (fp) {
-    var name = paths.basename(fp);
-    if (_fs.isEmptyFile(fp)) {
-      if(opts.verbose) {console.warn('Skipping empty file:'.yellow, fp);}
+  glob.find(filepath, opts).map(function (filepath) {
+    var name = _path.basename(filepath);
+    if (_fs.isEmptyFile(filepath)) {
+      if(opts.verbose) {console.warn('Skipping empty file:'.yellow, filepath);}
     } else {
       try {
         // If it's a string, try to require it.
-        contents = require(fp);
+        contents = require(filepath);
       } catch(e) {
-        // If it can't be required, try to read it directly.
-        contents = _fs.readDataSync(fp);
+        // If that doesn't work, try to read it directly.
+        contents = _fs.readDataSync(filepath);
       }
+      // `namespace` merges the data from each file into an object
+      // where the top-level property is the basename of the file itself
       if(opts.namespace === true) {
-        // Extend the data into an object named for the _fs.
+        // Extend the data into an object named for the file.
         opts.data[name] = _.cloneDeep(_.extend(opts.data, contents));
       } else if(opts.namespace === 'only') {
         opts.data[name] = _.cloneDeep(_.extend({}, opts.data, contents));
@@ -178,37 +262,10 @@ _fs.expandDataFiles = function (filepath, options) {
   return opts.data;
 };
 
-
-
-/**
- * fs stats
- */
-
-_fs.getStatsSync = function (filepath) {
-  try {
-    return fs.statSync(filepath);
-  } catch (err) {
-    err.message = 'Failed to retrieve "' + filepath + '" stats: ' + err.message;
-    throw err;
-  }
-};
-
-_fs.getStats = function (filepath, callback) {
-  try {
-    return fs.stat(filepath, callback);
-  } catch (err) {
-    err.message = 'Failed to retrieve "' + filepath + '" stats: ' + err.message;
-    return callback(err, null);
-  }
-};
-
-// Retrieve a specific file using globbing patterns. If
-// multiple matches are found, only the first is returned
-_fs.getFile = function(filepath, options) {
-  var str = glob.find(filepath, options)[0];
-  return str ? String(str) : null;
-};
-
+// Should "expandData" actually read in the files,
+// and "expandDataFiles" not? If so, we should deprecate
+// the latter and make this change.
+_fs.expandDataFiles = _fs.expandData;
 
 
 /**
@@ -228,8 +285,7 @@ _fs.mkdir = function (dest, callback) {
   });
 };
 
-// Make any directories and intermediate directories that
-// don't already exist
+// Make any dirs and intermediate dirs don't exist
 _fs.mkdirSync = function (dirpath, mode) {
   mode = mode || parseInt('0777', 8) & (~process.umask());
   if (!fs.existsSync(dirpath)) {
@@ -317,7 +373,7 @@ _fs.writeDataSync = function(dest, content, options) {
 
 
 /**
- * Copy
+ * Copy files
  */
 
 // Copy files synchronously and process any templates within
@@ -333,60 +389,10 @@ _fs.copyFileSync = function (src, dest, options) {
 
 
 /**
- * Remove
+ * Remove directories
  */
 
-// Remove any directories and child directories that exist
-_fs.rmdirSync = function () {
-  var dirpath = path.join.apply(path, arguments);
-  if (fs.existsSync(dirpath)) {
-    var files = fs.readdirSync(dirpath);
-    for (var i = 0, l = files.length; i < l; i++) {
-      var filepath = path.join(dirpath, files[i]);
-      if (filepath === "." || filepath === "..") {
-        continue;
-      } else if (fs.statSync(filepath).isDirectory()) {
-        _fs.rmdirSync(filepath);
-      } else {
-        fs.unlinkSync(filepath);
-      }
-    }
-    fs.rmdirSync(dirpath);
-  }
-};
-
-// _fs.delete was sourced and modified from grunt.file
-// https://github.com/gruntjs/grunt/blob/master/lib/grunt/file.js
-// https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
-//
-// Delete folders and files recursively
-_fs.delete = function(filepath, options) {
-  filepath = String(filepath);
-  options = options || {};
-
-  if (!_fs.exists(filepath)) {
-    console.warn('Cannot delete nonexistent file.');
-    return false;
-  }
-  // Only delete cwd or outside cwd if --force enabled. Be careful, people!
-  if (!options.force) {
-    if (paths.isPathCwd(filepath)) {
-      console.warn('Cannot delete the current working directory.');
-      return false;
-    } else if (!paths.isPathInCwd(filepath)) {
-      console.warn('Cannot delete files outside the current working directory.');
-      return false;
-    }
-  }
-  try {
-    // Actually delete. Or not.
-    rimraf.sync(filepath);
-    return true;
-  } catch(e) {
-    throw new Error('Unable to delete "' + filepath + '" file (' + e.message + ').', e);
-  }
-};
-
+// Asynchronously remove dirs and child dirs that exist
 _fs.rmdir = function (dirpath, callback) {
   if (!_.isFunction(callback)) {callback = function () {};}
   fs.readdir(dirpath, function (err, files) {
@@ -407,18 +413,80 @@ _fs.rmdir = function (dirpath, callback) {
   });
 };
 
+// Synchronously remove dirs and child dirs that exist
+_fs.rmdirSync = function () {
+  var dirpath = path.join.apply(path, arguments);
+  if (fs.existsSync(dirpath)) {
+    var files = fs.readdirSync(dirpath);
+    for (var i = 0, l = files.length; i < l; i++) {
+      var filepath = path.join(dirpath, files[i]);
+      if (filepath === "." || filepath === "..") {
+        continue;
+      } else if (fs.statSync(filepath).isDirectory()) {
+        _fs.rmdirSync(filepath);
+      } else {
+        fs.unlinkSync(filepath);
+      }
+    }
+    fs.rmdirSync(dirpath);
+  }
+};
+
+
 
 /**
- * Checks
+ * Delete files
  */
 
-// Does the filepath actually exist?
+/**
+ * _fs.delete was sourced and modified from grunt.file
+ * https://github.com/gruntjs/grunt/blob/master/lib/grunt/file.js
+ * https://github.com/gruntjs/grunt/blob/master/LICENSE-MIT
+ */
+
+// Delete folders and files recursively
+_fs.delete = function(filepath, options) {
+  var opts = _.extend({force: false}, options);
+  filepath = String(filepath);
+
+  if (!_fs.exists(filepath)) {
+    console.warn('Cannot delete nonexistent file.');
+    return false;
+  }
+  // Only delete cwd or outside cwd if options.force is true.
+  if (!opts.force) {
+    if (_path.isPathCwd(filepath)) {
+      console.warn('Cannot delete the current working directory.');
+      return false;
+    } else if (!_path.isPathInCwd(filepath)) {
+      console.warn('Cannot delete files outside the current working directory.');
+      return false;
+    }
+  }
+  try {
+    // Actually delete. Or not.
+    rimraf.sync(filepath);
+    return true;
+  } catch(err) {
+    var msg ='Unable to delete "' + filepath + '" file (' + err.message + ').';
+    throw new Error(msg, err);
+  }
+};
+
+
+
+
+/**
+ * Boolean checks
+ */
+
+// True if the filepath actually exist.
 _fs.exists = function() {
   var filepath = path.join.apply(path, arguments);
   return fs.existsSync(filepath);
 };
 
-// If the file actually exists, does it have any content?
+// True if the file exists and is NOT empty.
 _fs.isEmptyFile = function() {
   var filepath = path.join.apply(path, arguments);
   if (!_fs.exists(filepath)) {return false;}
@@ -426,16 +494,22 @@ _fs.isEmptyFile = function() {
   return (filepath.length === 0 || filepath === '') ? true : false;
 };
 
-// Is the path a directory?
+// True if the filepath is a directory.
 _fs.isDir = function() {
   var filepath = path.join.apply(path, arguments);
   if (!fs.existsSync(filepath)) {return false;}
   return fs.statSync(filepath).isDirectory();
 };
 
-// Is the path a file?
+// True if the filepath is a file.
 _fs.isFile = function() {
   var filepath = path.join.apply(path, arguments);
   if (!fs.existsSync(filepath)) {return false;}
   return fs.statSync(filepath).isFile();
+};
+
+// True if the filepath is a symbolic link.
+_fs.isLink = function() {
+  var filepath = path.join.apply(path, arguments);
+  return _fs.exists(filepath) && fs.lstatSync(filepath).isSymbolicLink();
 };
